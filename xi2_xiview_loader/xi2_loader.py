f8abc69b7ed655f97bad5c99608e592b893ee313
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2021-12-22 17:18:58 +0100

    fix dbinfo

--- xi2_xiview_loader/xi2_loader.py
+++ xi2_xiview_loader/xi2_loader.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import psycopg2  # todo - use sqlalchemy instead?
+import psycopg2  # todo - use sqlalchemy instead? LK: There's also flask_sqlalchemy
 import json
+from configparser import ConfigParser
 
 
 def create_app():
@@ -30,6 +31,27 @@ def create_app():
         }
     })
 
+    # https://www.postgresqltutorial.com/postgresql-python/connect/
+    def parse_database_info(filename='database.ini', section='postgresql'):
+        # create a parser
+        parser = ConfigParser()
+        # read config file
+        parser.read(filename)
+
+        # get section, default to postgresql
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+        return db
+
+    # read connection information
+    db_info = parse_database_info()
+
     @app.route('/get_data', methods=['GET'])
     def get_data():
         uuid = request.args.get('uuid')
@@ -40,12 +62,9 @@ def create_app():
         conn = None
         data = {}
         try:
-            # read connection parameters
-            params = app.config
-
             # connect to the PostgreSQL server
             print('Connecting to the PostgreSQL database...')
-            conn = psycopg2.connect(**params)
+            conn = psycopg2.connect(**db_info)
 
             # create a cursor
             cur = conn.cursor()
