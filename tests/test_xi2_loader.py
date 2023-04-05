695dcf9639d8736a4420b8381979c93e12dbce7d
Lars Kolbowski <lars.kolbowski@pm.me>
2023-04-05 17:28:41 +0200

    database connection error handling

--- tests/test_xi2_loader.py
+++ tests/test_xi2_loader.py
@@ -8,13 +8,30 @@ import os
 def app():
     fixtures_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
     config = os.path.join(fixtures_dir, 'test_db.ini')
-    app = create_app(config)
+    os.environ["DB_CONFIG"] = config
+    app = create_app()
     return app
 
 
-def test_general(client, config):
-    url = url_for('get_data')
-    # GET allowed
-    assert client.get(url)._status_code == 200
-    # check cors headers
-    assert config['CORS_HEADERS'] == 'Content-Type'
+# ToDo: write actual tests. Need to create a test database with some data to test against.
+# def test_index(client, config):
+#     url = url_for('index')
+#     assert client.get(url)._status_code == 200
+#
+#
+# def test_dataset(client, config):
+#     url = url_for('dataset', pxid='PXD000001')
+#     assert client.get(url)._status_code == 200
+#
+#
+# def test_get_data(client, config):
+#     url = url_for('get_data', id=1)
+#     # GET allowed
+#     assert client.get(url)._status_code == 200
+#     # check cors headers
+#     assert config['CORS_HEADERS'] == 'Content-Type'
+#
+#
+# def test_get_peaklist(client, config):
+#     url = url_for('get_peaklist', id=1)
+#     assert client.get(url)._status_code == 200
--- xi2_xiview_loader/xi2_loader.py
+++ xi2_xiview_loader/xi2_loader.py
@@ -4,9 +4,12 @@ import psycopg2  # todo - use sqlalchemy instead? LK: There's also flask_sqlalch
 import json
 import re
 from configparser import ConfigParser
+import os
 
 
-def get_db_connection(config='database.ini'):
+def get_db_connection():
+    config = os.environ.get('DB_CONFIG', 'database.ini')
+
     # https://www.postgresqltutorial.com/postgresql-python/connect/
     def parse_database_info(filename, section='postgresql'):
         # create a parser
@@ -61,9 +64,10 @@ def create_app():
         return render_template("datasets.html", datasets=datasets)
 
     def get_datasets():
-        """ Connect to the PostgreSQL database server """
+        """Get all datasets from the database."""
         conn = None
-        data = {}
+        ds_rows = []
+        error = None
         try:
             # connect to the PostgreSQL server
             conn = get_db_connection()
@@ -78,12 +82,15 @@ def create_app():
             print("finished")
             # close the communication with the PostgreSQL
             cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
+        except (Exception, psycopg2.DatabaseError) as e:
+            print(e)
+            error = e
         finally:
             if conn is not None:
                 conn.close()
                 print('Database connection closed.')
+            if error is not None:
+                raise error
             return ds_rows
 
     @app.route('/dataset', methods=['GET'])
@@ -95,7 +102,8 @@ def create_app():
     def get_dataset(pxid):
         """ Connect to the PostgreSQL database server """
         conn = None
-        data = {}
+        mzid_rows = []
+        error = None
         try:
             # connect to the PostgreSQL server
             print('Connecting to the PostgreSQL database...')
@@ -111,23 +119,33 @@ def create_app():
             print("finished")
             # close the communication with the PostgreSQL
             cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
+        except (Exception, psycopg2.DatabaseError) as e:
+            print(e)
+            error = e
         finally:
             if conn is not None:
                 conn.close()
                 print('Database connection closed.')
+            if error is not None:
+                raise error
             return mzid_rows
 
     @app.route('/get_data', methods=['GET'])
     def get_data():
         uuid = request.args.get('id')  # id(s) of data set(s)
-        # quit if uuid contains char that isn't alphanumeric, comma or hyphen
-        if not re.match(r'^[a-zA-Z0-9,-]+$', uuid):
+        # quit if uuid is None
+        if uuid is None:
+            return jsonify({"error": "No id provided"}), 400
+        # quit if uuid contains char that isn't alphanumeric, comma, hyphen, period, or tilde
+        elif not re.match(r'^[a-zA-Z0-9,-.~]+$', uuid):
             return jsonify({"error": "Invalid id(s)"}), 400
 
+        try:
+            data_object = get_data_object(uuid)
+        except psycopg2.DatabaseError:
+            return jsonify({"error": "Database error"}), 500
         # return json.dumps(get_data_object(uuid)) # think this will be more efficient as it doesn't pretty print
-        return jsonify(get_data_object(uuid))  # this is more readable for debugging
+        return jsonify(data_object)  # this is more readable for debugging
 
     @app.route('/get_peaklist', methods=['GET'])
     def get_peaklist():
@@ -214,6 +232,7 @@ def create_app():
         """ Connect to the PostgreSQL database server """
         conn = None
         data = {}
+        error = None
         try:
             # connect to the PostgreSQL server
             conn = get_db_connection()
@@ -231,18 +250,21 @@ def create_app():
             print("finished")
             # close the communication with the PostgreSQL
             cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
+        except (Exception, psycopg2.DatabaseError) as e:
+            error = e
         finally:
             if conn is not None:
                 conn.close()
                 print('Database connection closed.')
+            if error is not None:
+                raise error
             return data
 
     def get_peaklist_object(spectrum_uuid):
         """ Connect to the PostgreSQL database server """
         conn = None
-        data = {}
+        data = []
+        error = None
         try:
             # connect to the PostgreSQL server
             conn = get_db_connection()
@@ -257,93 +279,16 @@ def create_app():
             print("finished")
             # close the communication with the PostgreSQL
             cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
+        except (Exception, psycopg2.DatabaseError) as e:
             print(error)
+            error = e
         finally:
             if conn is not None:
                 conn.close()
                 print('Database connection closed.')
+            if error is not None:
+                raise error
             return data
-
-#     @app.route('/projects/<project_id>/sequences', methods=['GET'])
-#     def sequences(project_id):
-#         """
-#         Get all sequences belonging to a project.
-#
-#         :param project_id: identifier of a project, for ProteomeXchange projects this is the PXD****** accession
-#         :return: JSON object with all dbref id, mzIdentML file it came from and sequences
-#         """
-#         conn = None
-#         mzid_rows = []
-#         try:
-#             # connect to the PostgreSQL server and create a cursor
-#             print('Connecting to the PostgreSQL database...')
-#             conn = psycopg2.connect(**db_info)
-#             cur = conn.cursor(cursor_factory=RealDictCursor)
-#
-#
-#             sql = """SELECT s.id, u.identification_file_name, s.sequence
-#                         FROM upload AS u
-#                         JOIN dbsequence AS s ON u.id = s.upload_id
-#                      WHERE u.project_id = %s;"""
-#
-#             print(sql)
-#             cur.execute(sql, [project_id])
-#             mzid_rows = cur.fetchall()
-#
-#             print("finished")
-#             # close the communication with the PostgreSQL
-#             cur.close()
-#         except (Exception, psycopg2.DatabaseError) as error:
-#             print(error)
-#         finally:
-#             if conn is not None:
-#                 conn.close()
-#                 print('Database connection closed.')
-#             return jsonify(mzid_rows)
-#
-#     @app.route('/projects/<project_id>/residue-pairs/psm-level', methods=['GET'])
-#     def get_psm_level_residue_pairs(project_id):
-#         """
-#         Get all residue pairs (based on PSM level data) belonging to a project.
-#
-#         There will be multiple entries for identifications with positional uncertainty of peptide in protein sequences.
-#         :param project_id: identifier of a project, for ProteomeXchange projects this is the PXD****** accession
-#         :return:
-#         """
-#         conn = None
-#         mzid_rows = []
-#         try:
-#             # connect to the PostgreSQL server and create a cursor
-#             print('Connecting to the PostgreSQL database...')
-#             conn = psycopg2.connect(**db_info)
-#             cur = conn.cursor(cursor_factory=RealDictCursor)
-#
-#             sql = """WITH upload_ids AS (SELECT ID from upload WHERE project_id = %s)
-# SELECT si.id, u.identification_file_name,
-# si.pep1_id, /*pe1.pep_start as pep1_start, mp1.link_site1 as pep1_site,*/ pe1.dbsequence_ref as pep1_prot_id, (pe1.pep_start + mp1.link_site1 - 1) as abspos1,
-# si.pep2_id, /* pe2.pep_start as pep2_start, mp2.link_site1 as pep2_site, */ pe2.dbsequence_ref as pep2_prot_id, (pe2.pep_start + mp2.link_site1 - 1) as abspos2 FROM
-# (SELECT * from spectrumidentification where upload_id in (select * from upload_ids) ) si INNER JOIN
-# (SELECT * from modifiedpeptide where upload_id in (select * from upload_ids) and link_site1 is not null ) mp1 ON si.pep1_id = mp1.id AND si.upload_id = mp1.upload_id INNER JOIN
-# (SELECT * from peptideevidence where upload_id in (select * from upload_ids) ) pe1 ON mp1.id = pe1.peptide_ref AND mp1.upload_id = pe1.upload_id INNER JOIN
-# (SELECT * from modifiedpeptide where upload_id in (select * from upload_ids)  and link_site1 is not null ) mp2 ON si.pep2_id = mp2.id AND si.upload_id = mp2.upload_id INNER JOIN
-# (SELECT * from peptideevidence where upload_id in (select * from upload_ids) ) pe2 ON mp2.id = pe2.peptide_ref AND mp2.upload_id = pe2.upload_id INNER JOIN
-# (SELECT identification_file_name, id from upload WHERE project_id = %s) u on u.id = si.upload_id;"""
-#
-#             print(sql)
-#             cur.execute(sql, [project_id, project_id])
-#             mzid_rows = cur.fetchall()
-#
-#             print("finished")
-#             # close the communication with the PostgreSQL
-#             cur.close()
-#         except (Exception, psycopg2.DatabaseError) as error:
-#             print(error)
-#         finally:
-#             if conn is not None:
-#                 conn.close()
-#                 print('Database connection closed.')
-#             return jsonify(mzid_rows)
 
     return app
 
